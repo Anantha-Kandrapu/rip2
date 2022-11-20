4326a417983377e49257e1b08a6a83059a1965bc
ARK <akandrap@asu.edu>
2022-11-19 23:03:03 -0700

    rip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { Redirect } from 'react-router-dom'
 
-function App() {
+function Page3() {
+  const count = useRef(0);
+  const navigate = useNavigate();
+  const [currCount, setCount] = useState(count);
+
+  return (
+    <div className='App'>
+      <h2> Welcome to Page 3</h2>
+      <button onClick={() => { count.current++; console.log(count); setCount(count.current) }}> Hey Add </button>
+      <button onClick={() => { count.current--; console.log(count); setCount(count.current) }}> Hey Minus </button>
+      <h2 data-testid='madCount'>count : {count.current}</h2>
+      <button data-testid='goBackP2' onClick={() => navigate('/Page2')} >go to P2</button>
+    </div>
+  )
+}
+function Page2() {
+  const count = useRef(0);
+  const navigate = useNavigate();
+  const [currCount, setCount] = useState(count);
+
+  return (
+    <div className='App'>
+      <h2> Welcome to Page 2</h2>
+      <button onClick={() => { count.current++; console.log(count); setCount(count.current) }}> Hey Add </button>
+      <button onClick={() => { count.current--; console.log(count); setCount(count.current) }}> Hey Minus </button>
+      <h2 data-testid='madCount'>count : {count.current}</h2>
+      <button data-testid='goToP3' onClick={() => navigate('/Page3')}>go to P3</button>
+      <button data-testid='goBackP1' onClick={() => navigate('/')} >go to P1</button>
+    </div>
+  )
+}
+function Page1() {
+  const count = useRef(0);
+  const [currCount, setCount] = useState(count);
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>wambam This is page 1</h2>
+      <button onClick={() => { count.current++; console.log(count); setCount(count.current) }}> Hey Add </button>
+      <button onClick={() => { count.current--; console.log(count); setCount(count.current) }}> Hey Minus </button>
+      <h2 data-testid='madCount'>count : {count.current}</h2>
+      <button data-testid='goToP2' onClick={() => navigate('/Page2')}>go to P2</button>
     </div>
+  );
+}
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Page1></Page1>}></Route>
+        <Route path='/Page2' element={<Page2></Page2>}></Route>
+        <Route path='/Page3' element={<Page3></Page3>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wambam = screen.getByText(/wambam/i);
+  expect(wambam).toBeInTheDocument();
+  const add = screen.getByText('Hey Add');
+  const minus = screen.getByText(/Hey minus/i);
+  const count = screen.getByTestId('madCount');
+  expect(count).toBeInTheDocument();
+  expect(add).toBeInTheDocument();
+  expect(minus).toBeInTheDocument();
+  fireEvent.click(add);
+  expect(screen.getByText(/count : 1/i)).toBeInTheDocument();
+  fireEvent.click(minus);
+  expect(screen.getByText(/count : 0/i)).toBeInTheDocument();
+});
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const wambam = screen.getByText(/wambam/i);
+  expect(wambam).toBeInTheDocument();
+  const add = screen.getByText('Hey Add');
+  const minus = screen.getByText(/Hey minus/i);
+  const count = screen.getByTestId('madCount');
+  expect(count).toBeInTheDocument();
+  expect(add).toBeInTheDocument();
+  expect(minus).toBeInTheDocument();
+  fireEvent.click(add);
+  expect(screen.getByText(/count : 1/i)).toBeInTheDocument();
+  fireEvent.click(minus);
+  expect(screen.getByText(/count : 0/i)).toBeInTheDocument();
 });
